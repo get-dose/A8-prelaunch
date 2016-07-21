f2a613abfac718ac162dcf836d41e6e72b014b70
ray-curran <rpcurr@gmail.com>
2016-07-20 19:20:26 -0500

    Complete initial page 2 design for web, added recent bootstrap for basic responsiveness, deleted larger files to lower weight of application, fixed logic error

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,28 +16,28 @@ class User < ActiveRecord::Base
   REFERRAL_STEPS = [
     {
       'count' => 5,
-      'html' => 'Shave<br>Cream',
+      'html' => 'Dose shaker<br>bottle',
       'class' => 'two',
       'image' =>  ActionController::Base.helpers.asset_path(
         'refer/cream-tooltip@2x.png')
     },
     {
       'count' => 10,
-      'html' => 'Truman Handle<br>w/ Blade',
+      'html' => '1 bottle<br>A8',
       'class' => 'three',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/truman@2x.png')
     },
     {
       'count' => 25,
-      'html' => 'Winston<br>Shave Set',
+      'html' => 'DOSE Baseball<br>hat',
       'class' => 'four',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/winston@2x.png')
     },
     {
       'count' => 50,
-      'html' => 'One Year<br>Free Blades',
+      'html' => 'One Year 15%<br>discount on A8',
       'class' => 'five',
       'image' => ActionController::Base.helpers.asset_path(
         'refer/blade-explain@2x.png')
