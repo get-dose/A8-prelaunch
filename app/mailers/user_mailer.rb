2b04aa70136291f6703a39e79d173199600051d4
ray-curran <rpcurr@gmail.com>
2016-07-21 21:13:36 -0500

    Clean up title, favicons, twitter/facebook messages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def signup_email(user)
     @user = user
-    @twitter_message = "#Shaving is evolving. Excited for @harrys to launch."
+    @twitter_message = "A8 is upgrading nutrition. Didn’t want to leave you all behind. Check out A8 here."
 
     mail(:to => user.email, :subject => "Thanks for signing up!")
   end
