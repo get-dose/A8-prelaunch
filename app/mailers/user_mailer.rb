9bf425b3036f17216a150954fb24a04998d73f76
[01;31m[K"name"[m[K: "Sabri Helal <[01;31m[K"email"[m[K: null>
2016-07-23 21:37:16 -0500

    Test facebook share

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ActionMailer::Base
   def signup_email(user)
     @user = user
     @twitter_message = "A8 is upgrading nutrition. Didn’t want to leave you all behind. Check out A8 here."
+    @facebook_message = "A8 is upgrading nutrition. Didn’t want to leave you all behind. Check out A8 here."
 
     mail(:to => user.email, :subject => "Thanks for signing up!")
   end
